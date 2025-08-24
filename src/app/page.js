834ccef35e9e25d7479a61d9dde5d9e68ed374a4
sadeeshaChiran <pmsckaushalya@gmail.com>
2025-08-24 14:06:34 +0530

    Remove unused components and styles to streamline the codebase

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,19 @@
-
-
+import GalleryCarousel from "@/app/GalleryCarousel"
+import './page.css'
 
 export default function Home() {
+  const sections = [
+    { id: "page1", label: "Sinhala" },
+    { id: "page2", label: "English" },
+    { id: "page3", label: "Date" },
+  ]
+
   return (
-    <>
-     <div>
-   
-      
+    <div className="root">
+
+      <GalleryCarousel />
     </div>
-    </>
-  );
+  )
 }
+
+
